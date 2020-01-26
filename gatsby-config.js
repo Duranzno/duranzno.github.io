da272eb75194cfb0453c400fb719cd4d3c17ff0e
Alejandro <aledurax@gmail.com>
2020-01-26 12:24:36 -0400

    [master] Cleaned Boilerplate

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+const {
+  siteMetadata, 
+  pluginManifest:options
+} = require('./config')
+
+module.exports = {
+  siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options,
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
+}
