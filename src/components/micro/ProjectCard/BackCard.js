5674a231c5cd5a3364f2f25dbcb96f873bf45696
Alejandro <aledurax@gmail.com>
2020-02-07 09:22:08 -0400

    [develop] Improved BackCard

--- src/components/micro/ProjectCard/BackCard.js
+++ src/components/micro/ProjectCard/BackCard.js
@@ -2,34 +2,33 @@ import React from 'react'
 import styled from '@emotion/styled'
 import { Text, Image, Box } from 'theme-ui'
 import { ProjectPropTypes, StyledCard } from './ProjectCard.styles'
+import { SocialLink } from '../index'
 
 export const StyledBack = styled(StyledCard)`
   z-index: 1000;
   background: #ffffff;
   display: inline-block;
-  margin: 8px;
   max-width: 300px;
   text-align: left;
   /* transition: all 0.3s 0s; */
   /* transform: ${props => (props.flipped ? ' rotateY(-180deg)' : 'rotateX(0deg) rotateY(0deg)')}; */
 `
-// https://codepen.io/candroo/full/wKEwRL
 export const Back = ({ project, flipped }) => {
+  console.log(project)
   return (
     <StyledBack flipped={flipped} className="back-card">
       <Image src={project.screenshots[0].file.url} sx={{ maxWidth: '300px' }} />
-      <Box sx={{ display: 'flex', direction: '' }} />
-      <Text sx={{ fontSize: 4, fontWeight: 'bold' }} variant="heading">
-        {project.name}
-      </Text>
-      <Text sx={{ fontSize: 3 }}>{project.description}</Text>
-
-      <div>
-        {project.stack.slice(0, 3).map(({ id, IconifyName }) => (
-          <>{IconifyName}</>
-          // <IconifyIcon IconifyName={IconifyName} size={6} key={id} />
-        ))}
-      </div>
+      <Box sx={{ display: 'flex', flexDirection: 'column', px: 3, pb: 1 }}>
+        <Text sx={{ fontSize: 4, fontWeight: 'bold' }} variant="heading">
+          {project.name}
+        </Text>
+        <Text sx={{ fontSize: 3 }}>{project.description}</Text>
+        <div>
+          {project.stack.slice(0, 3).map(({ id, iconifyName }) => (
+            <SocialLink IconifyName={iconifyName} sx={{ width: 2, height: 2 }} key={id} color="black" />
+          ))}
+        </div>
+      </Box>
     </StyledBack>
   )
 }
