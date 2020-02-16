d1189d7a2c4c3a53a74b6b465f7b08e91cf86353
Alejandro <aledurax@gmail.com>
2020-02-16 15:18:53 -0400

    [develop] Created Blog & Pages

--- src/common/Background/TechBackground.js
+++ src/common/Background/TechBackground.js
@@ -4,10 +4,7 @@ import styled from '@emotion/styled'
 import { graphql, useStaticQuery } from 'gatsby'
 import { randomizePosition, useWindowSize } from '@utils'
 import { IconifyIcon } from '@components'
-import {
-  upDownAnimation,
-  upDownWideAnimation,
-} from '../common.styles.animations'
+import { upDownAnimation, upDownWideAnimation } from '../common.styles.animations'
 
 export const TechBackground = () => {
   const dimensions = useWindowSize()
@@ -15,22 +12,22 @@ export const TechBackground = () => {
     allContentfulStackLink: { edges },
   } = useStaticQuery(query)
   return null
-  return (
-    <Bg className="tech-background">
-      {edges.map(e => {
-        const {
-          node: { id, iconifyName, name },
-        } = e
-        const { x, y } = randomizePosition(dimensions)
-        const sx = {
-          size: Math.floor(Math.random() * 3) + 4,
-          animation: Math.random() ? upDownAnimation : upDownWideAnimation,
-        }
-        const iconifyProps = { id, iconifyName, name, url: name, sx }
-        return <IconifyIcon animated x={x} y={y} key={id} {...iconifyProps} />
-      })}
-    </Bg>
-  )
+  // return (
+  //   <Bg className="tech-background">
+  //     {edges.map(e => {
+  //       const {
+  //         node: { id, iconifyName, name },
+  //       } = e
+  //       const { x, y } = randomizePosition(dimensions)
+  //       const sx = {
+  //         size: Math.floor(Math.random() * 3) + 4,
+  //         animation: Math.random() ? upDownAnimation : upDownWideAnimation,
+  //       }
+  //       const iconifyProps = { id, iconifyName, name, url: name, sx }
+  //       return <IconifyIcon animated x={x} y={y} key={id} {...iconifyProps} />
+  //     })}
+  //   </Bg>
+  // )
 }
 const query = graphql`
   query UsedStack {
--- src/gatsby/createPages.js
+++ src/gatsby/createPages.js
@@ -3,13 +3,9 @@ const { paginate } = require(`gatsby-awesome-pagination`)
 const config = require('../../gatsby-config')
 const query = require('./query')
 
-module.exports = async ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  const basePath = config.siteMetadata.siteUrl || '/'
-
-  // Create a page for each "post"
+module.exports = async ({ graphql, actions: { createPage } }) => {
   const postsQuery = await graphql(query.data.posts)
+  const basePath = '/'
   const posts = postsQuery.data.allContentfulBlogPost.edges
   posts.forEach((post, i) => {
     const next = i === posts.length - 1 ? null : posts[i + 1].node
@@ -17,7 +13,7 @@ module.exports = async ({ graphql, actions }) => {
     const postName = post.node.slug.toLowerCase()
 
     createPage({
-      path: `${basePath === '/' ? '' : basePath}/${postName}/`,
+      path: `${basePath === '/' ? '/' : basePath}blog/${postName}`,
       component: path.resolve(`./src/templates/blog-post.js`),
       context: {
         slug: post.node.slug,
@@ -46,9 +42,8 @@ module.exports = async ({ graphql, actions }) => {
   const stacksQuery = await graphql(query.data.stacks)
   const stacks = stacksQuery.data.allContentfulStackLink.edges
   stacks.forEach(stack => {
-    const stackPagination =
-      basePath === '/' ? `/tag/${name}` : `/${basePath}/tag/${name}`
     const name = stack.node.name.toLowerCase()
+    const stackPagination = basePath === '/' ? `/tag/${name}` : `/${basePath}/tag/${name}`
 
     paginate({
       createPage,
@@ -64,3 +59,65 @@ module.exports = async ({ graphql, actions }) => {
     })
   })
 }
+
+// module.exports = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+
+//   const basePath = config.siteMetadata.siteUrl || '/'
+
+//   // Create a page for each "post"
+//   const postsQuery = await graphql(query.data.posts)
+//   const posts = postsQuery.data.allContentfulBlogPost.edges
+//   posts.forEach((post, i) => {
+//     const next = i === posts.length - 1 ? null : posts[i + 1].node
+//     const prev = i === 0 ? null : posts[i - 1].node
+//     const postName = post.node.slug.toLowerCase()
+
+//     createPage({
+//       path: `${basePath === '/' ? '' : basePath}/${postName}/`,
+//       component: path.resolve(`./src/templates/blog-post.js`),
+//       context: {
+//         slug: post.node.slug,
+//         basePath: basePath === '/' ? '' : basePath,
+//         prev,
+//         next,
+//       },
+//     })
+//   })
+
+//   // Create a page containing all "posts" and paginate.
+//   paginate({
+//     createPage,
+//     component: path.resolve(`./src/templates/posts.js`),
+//     items: posts,
+//     itemsPerFirstPage: config.siteMetadata.postsPerFirstPage || 7,
+//     itemsPerPage: config.siteMetadata.postsPerPage || 6,
+//     pathPrefix: basePath,
+//     context: {
+//       basePath: basePath === '/' ? '' : basePath,
+//       paginationPath: basePath === '/' ? '' : `/${basePath}`,
+//     },
+//   })
+
+//   // Create "stack" page and paginate
+//   const stacksQuery = await graphql(query.data.stacks)
+//   const stacks = stacksQuery.data.allContentfulStackLink.edges
+//   stacks.forEach(stack => {
+//     const stackPagination =
+//       basePath === '/' ? `/tag/${name}` : `/${basePath}/tag/${name}`
+//     const name = stack.node.name.toLowerCase()
+
+//     paginate({
+//       createPage,
+//       component: path.resolve(`./src/templates/stack.js`),
+//       items: stack.node.post || [],
+//       itemsPerPage: config.siteMetadata.postsPerPage || 6,
+//       pathPrefix: stackPagination,
+//       context: {
+//         name: stack.node.name,
+//         basePath: basePath === '/' ? '' : basePath,
+//         paginationPath: stackPagination,
+//       },
+//     })
+//   })
+// }
